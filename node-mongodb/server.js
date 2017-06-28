d2ae9e11007f20881c11f4bd4ae6351900271e5e
Murali Mankhala <Murali Mankhala@AXATech-IN.INTRAXA>
2017-06-28 15:14:12 +0530

    mongoose

--- node-mongodb/server.js
+++ node-mongodb/server.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient,
 var dboper = require('./operations');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/Angular';
+var url = 'mongodb://localhost:27017/mongodb';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
