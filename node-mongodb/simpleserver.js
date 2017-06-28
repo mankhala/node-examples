dca6cea7ba4e88eed9ac8bbc7b4bd6fbfbab9c98
mankhala <mankhala@yahoo.com>
2017-06-28 11:08:23 +0530

    Node and MongoDb Communication

--- node-mongodb/simpleserver.js
+++ node-mongodb/simpleserver.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/conFusion';
+var url = 'mongodb://localhost:27017/mongodb';
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
     assert.equal(err,null);
